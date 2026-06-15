c1d045629ddaaa026a3eb2b82661ec128115cd40
Kanumul Deniya Liyanage Sanidu Harsha <saniduharsha2002@gmail.com>
2026-06-15 14:43:13 +0530

    Create and display main game window

---
+++ Main/src/game/Game.java
@@ -0,0 +1,28 @@
+package game;
+import javax.swing.*;
+
+public class Game {
+
+    JFrame frame = new JFrame();
+    JLabel textLabel = new JLabel();
+    JPanel textPanel = new JPanel();
+
+    JButton[] board = new JButton[9];
+    JPanel boardPanel = new JPanel();
+
+
+
+    public Game() {
+        //JFrame
+        frame.setVisible(true);
+        frame.setSize(600, 650);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Tic Tac Toe");
+
+
+
+
+    }
+}
